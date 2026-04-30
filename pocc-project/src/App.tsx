93304e4cb66fea12436cabe1ef65a61d9acef24a
Liong <axel.tanzil@student.citrakasih.sch.id>
2026-04-30 15:39:12 +0800

    add pages

--- pocc-project/src/App.tsx
+++ pocc-project/src/App.tsx
@@ -1,12 +1,4 @@
+import Item from "./pages/Item"
 function App() {
-  return (
-  <div>
-    <h1>Menu</h1>
-    <p>Makanan</p>
-    <p>Minuman</p>
-    <p>Cemilan</p>
-  </div>
-  )
-}
-
-export default App;
+  return <Item/>
+} export default App;
---
+++ pocc-project/src/pages/item.tsx
@@ -0,0 +1,16 @@
+import {
+    ScatterChart,
+    Scatter,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+} from "recharts";
+
+function Item() {
+    return (
+        <div>
+            <h1>Regresi Linear - By Joshua</h1>
+        </div>
+    )
+} export default Item;
